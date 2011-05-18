b6f5dce49390152eaf0cc0d23e2e2489c959496f
elishowk <elishowk@nonutc.fr>
2011-05-18 18:22:40 +0200

    dependencies

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup, find_packages
 import sys, os
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
-NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+README = open(os.path.join(here, 'README')).read()
 
 
 version = '0.1'
 
 install_requires = [
-    # List your project dependencies here.
+    'bottle',
+    'riak',
+    'protobuf',
+    'werkzeug'
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
 ]
@@ -18,21 +20,25 @@ install_requires = [
 setup(name='commonecouteserver',
     version=version,
     description="Core CommOnEcoute Server",
-    long_description=README + '\n\n' + NEWS,
+    long_description=README,
     classifiers=[
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Server"
       # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     ],
-    keywords='http web',
+    keywords='http web server riak',
     author='Elias Showk',
     author_email='elishowk@nonutc.fr',
     url='http://commonecoute.com',
     license='GNU AGPL v3',
     packages=find_packages('src'),
-    package_dir = {'': 'src'},include_package_data=True,
+    package_dir = {'': 'src'},
+    include_package_data=True,
+    scripts = ['bin/server.py'],
     zip_safe=False,
     install_requires=install_requires,
-    entry_points={
-        'console_scripts':
-            ['commonecouteserver=commonecouteserver:main']
-    }
+    #entry_points={
+    #    'console_scripts':
+    #        ['commonecouteserver=commonecouteserver:main']
+    #}
 )
