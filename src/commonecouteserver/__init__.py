3a070807523fb38ee4b194999c6da1c2728b3b0c
elishowk <elishowk@nonutc.fr>
2011-05-20 18:38:55 +0200

    removed trailing slashes from routes

--- src/commonecouteserver/__init__.py
+++ src/commonecouteserver/__init__.py
@@ -49,118 +49,118 @@ def _request_body(request):
     return json.loads(data)
 
 # GET handlers
-@coeserver.get('/track/:id/')
+@coeserver.get('/track/:id')
 def get_track(id):
     return coebuckets['track'].read(id)
     
-@coeserver.get('/event/:id/')
+@coeserver.get('/event/:id')
 def get_event(id):
     return coebuckets['event'].read(id)
     
-@coeserver.get('/user/:id/')
+@coeserver.get('/user/:id')
 def get_user(id):
     return coebuckets['user'].read(id)
     
-@coeserver.get('/post/:id/')
+@coeserver.get('/post/:id')
 def get_post(id):
     return coebuckets['post'].read(id)
 
-@coeserver.get('/genre/:id/')
+@coeserver.get('/genre/:id')
 def get_genre(id):
     return coebuckets['genre'].read(id)
     
-@coeserver.get('/product/:id/')
+@coeserver.get('/product/:id')
 def get_product(id):
     return coebuckets['product'].read(id)
  
-@coeserver.get('/artist/:id/')
+@coeserver.get('/artist/:id')
 def get_artist(id):
     return coebuckets['artist'].read(id)
    
 # POST handlers
-@coeserver.post('/track/')
+@coeserver.post('/track')
 def post_track():
     coebuckets['track'].create(_request_body(request))
     
-@coeserver.post('/event/')
+@coeserver.post('/event')
 def post_event():
     coebuckets['event'].create(_request_body(request))
     
-@coeserver.post('/user/')
+@coeserver.post('/user')
 def post_user():
     coebuckets['user'].create(_request_body(request))
     
-@coeserver.post('/post/')
+@coeserver.post('/post')
 def post_post():
     coebuckets['post'].create(_request_body(request))
 
-@coeserver.post('/genre/')
+@coeserver.post('/genre')
 def post_genre():
     coebuckets['genre'].create(_request_body(request))
     
-@coeserver.post('/product/')
+@coeserver.post('/product')
 def post_product():
     coebuckets['product'].create(_request_body(request))
     
-@coeserver.post('/artist/')
+@coeserver.post('/artist')
 def post_artist():
     return coebuckets['artist'].create(_request_body(request))
     
 # PUT handlers
-@coeserver.put('/track/:id/')
+@coeserver.put('/track/:id')
 def put_track(id):
     return coebuckets['track'].update(id, _request_body(request))
     
-@coeserver.put('/event/:id/')
+@coeserver.put('/event/:id')
 def put_event(id):
     return coebuckets['event'].update(id, _request_body(request))
     
-@coeserver.put('/user/:id/')
+@coeserver.put('/user/:id')
 def put_user(id):
     return coebuckets['user'].update(id, _request_body(request))
     
-@coeserver.put('/post/:id/')
+@coeserver.put('/post/:id')
 def put_post(id):
     return coebuckets['post'].update(id, _request_body(request))
 
-@coeserver.put('/genre/:id/')
+@coeserver.put('/genre/:id')
 def put_genre(id):
     return coebuckets['genre'].update(id, _request_body(request))
     
-@coeserver.put('/product/:id/')
+@coeserver.put('/product/:id')
 def put_product(id):
     return coebuckets['product'].update(id, _request_body(request))
 
-@coeserver.put('/artist/:id/')
+@coeserver.put('/artist/:id')
 def put_artist(id):
     return coebuckets['artist'].update(id, _request_body(request))
   
 # DELETE handlers
-@coeserver.delete('/track/:id/')
+@coeserver.delete('/track/:id')
 def delete_track(id):
     coebuckets['track'].delete(id)
     
-@coeserver.delete('/event/:id/')
+@coeserver.delete('/event/:id')
 def delete_event(id):
     coebuckets['event'].delete(id)
     
-@coeserver.delete('/user/:id/')
+@coeserver.delete('/user/:id')
 def delete_user(id):
     coebuckets['user'].delete(id)
     
-@coeserver.delete('/post/:id/')
+@coeserver.delete('/post/:id')
 def delete_post(id):
     coebuckets['post'].delete(id)
 
-@coeserver.delete('/genre/:id/')
+@coeserver.delete('/genre/:id')
 def delete_genre(id):
     coebuckets['genre'].delete(id)
     
-@coeserver.delete('/product/:id/')
+@coeserver.delete('/product/:id')
 def delete_product(id):
     coebuckets['product'].delete(id)  
 
-@coeserver.delete('/artist/:id/')
+@coeserver.delete('/artist/:id')
 def delete_artist(id):
     coebuckets['artist'].delete(id)  
 
