76fb18bfa9c2643a215e4336928e79c444fed450
elishowk <elishowk@nonutc.fr>
2011-05-24 18:20:18 +0200

    revised test data

--- bin/testserver.py
+++ bin/testserver.py
@@ -20,15 +20,25 @@ import webob
 import json
 from datetime import datetime
 import urllib
+import uuid
 
 class COEserverTestCase(unittest.TestCase):
     data = {
         'event': {
-            'id_txt': "http://commonecoute.com/urleventunique.html",
+            'id_txt': "%s"%uuid.uuid4(),
+            'url_txt': "http://commonecoute.com/urleventunique.html",
+            'title_txt': "Amon Tobin presents ISAM @ Le Bataclan, Paris",
+            'location_txt': "Le Bataclan, Paris",
+            'image': "http://commonecoute.com/assets/images/track/0125.png",
+            'views': 0,
+            'overview_txt': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            'start_date': str(datetime.utcnow().isoformat()),
+            'end_date': str(datetime.now().isoformat()),
             'link_track': ["http://commonecoute.com/urltrackunique.html"]
         },
         'track': {
-            'id_txt': "http://commonecoute.com/urltrackunique.html",
+            'url_txt': "http://commonecoute.com/urltrackunique.html",
+            'id_txt': "%s"%uuid.uuid4(),
             'title_txt': "Amon Tobin presents ISAM @ Le Bataclan, Paris",
             'views': 0,
             'video': "http://commonecoute.com/videoembedurl.mkv",
@@ -41,13 +51,13 @@ class COEserverTestCase(unittest.TestCase):
             'start_date': str(datetime.utcnow().isoformat()),
             'end_date': str(datetime.now().isoformat()),
             'link_artist': ["http://commonecoute.com/urlartistunique.html", "http://commonecoute.com/urlartistunique2.html"],
-            'link_genre': ["http://commonecoute.com/urlelectronicunique.html"]
+            'link_genre': ["http://commonecoute.com/urlelectronicunique.html"] 
         },
         'user': {
             'id_txt': 'elishowk@nonutc.fr',
             'firstname_txt':'elias',
             'lastname_txt': 'showk',
-            'image': 'http://gravatar.com/elishowk',
+            'image': 'http://gravatar.com/elishowk@nonutc.fr',
             'genre': 'M',
             'birthday_date': "1984-07-18",
             'city_txt': "Paris",
@@ -57,7 +67,8 @@ class COEserverTestCase(unittest.TestCase):
             'link_artist': ["http://commonecoute.com/urlartistunique.html"]
         },
         'product': {
-            'id_txt': "http://commonecoute.com/urlproduct1.html",
+            'id_txt': "%s"%uuid.uuid4(),
+            'url_txt': "http://commonecoute.com/urlproduct1.html",
             'title_txt': "ISAM",
             'release_date': "2011-03-28",
             'overview_txt': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
@@ -65,18 +76,20 @@ class COEserverTestCase(unittest.TestCase):
             'link_artist': ["http://commonecoute.com/urlartistunique.html"]
         },
         'artist': {
-            'id_txt': "http://commonecoute.com/urlartistunique.html",
+            'id_txt': "%s"%uuid.uuid4(),
+            'url_txt': "http://commonecoute.com/urlartistunique.html",
             'name': "Amon Tobin",
             'link_product': ["http://commonecoute.com/urlproduct1.html", "http://commonecoute.com/urlproduct2.html"],
             'image': "http://commonecoute.com/assets/images/artist/025.png",
             'link_genre': ["http://commonecoute.com/urlelectronicunique.html"]
         },
         'genre': {
-            'id_txt': "http://commonecoute.com/urlelectronicunique.html",
+            'id_txt': "%s"%uuid.uuid4(),
+            'url_txt': "http://commonecoute.com/urlelectronicunique.html",
             'name': "electronic"
         },
-        'post':  {
-            #'id_txt': generated uuid4
+        'post': {
+            'id_txt': "%s"%uuid.uuid4(),
             'link_track': ["http://commonecoute.com/urltrackunique.html"],
             'timingstart_date': str(datetime.utcnow().isoformat()),
             'timingend_date': str(datetime.utcnow().isoformat()),
