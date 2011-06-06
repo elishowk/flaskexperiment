03dcb7fd95df0800a8f2982b3b9814f53b39e479
elishowk <elishowk@nonutc.fr>
2011-06-06 19:12:01 +0200

    moved data to a submodule

--- commonecouteserver/data/__init__.py
+++ commonecouteserver/data/__init__.py
@@ -13,7 +13,7 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/agpl.html>
 
-from flask import abort, make_response, json
+from flask import abort
 
 import riak
 import uuid
